a68c453d21fa170bfd8687613ccb1c51f8350db3
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2025-06-09 09:45:47 -0400

    python init
    Co-authored-by: github153588 <github153588@users.noreply.github.com>

---
+++ AI.py
@@ -0,0 +1,5 @@
+def ai(paddle_frect, other_paddle_frect, ball_frect, table_size):
+    if paddle_frect.pos[1] + paddle_frect.size[1] / 2 < ball_frect.pos[1] + ball_frect.size[1] / 2:
+        return "down"
+    else:
+        return "up"
